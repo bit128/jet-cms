497720add335949c5c52abc35e5a4981e1df2457
hongbo <hongbo@hb-mbp15.local>
2019-04-10 17:51:53 +0800

    内容自动保存

--- src/main/java/com/lanxin/pandora/controllers/HomeController.java
+++ src/main/java/com/lanxin/pandora/controllers/HomeController.java
@@ -10,22 +10,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/home")
 public class HomeController {
 
+    /**
+     * 后台首页面
+     * @return
+     */
     @RequestMapping("")
     public String index() {
         return "home/index";
     }
 
+    /**
+     * 栏目管理 - 页面
+     * @return
+     */
     @RequestMapping("/channel")
     public String channel() {
         return "home/channel";
     }
 
+    /**
+     * 内容管理页面
+     * @param id
+     * @param data
+     * @return
+     */
     @RequestMapping(value = "/content/id/{id}")
     public String content(@PathVariable String id, ModelMap data) {
         data.put("cid", id);
         return "home/content";
     }
 
+    /**
+     * 资源管理 - 页面
+     * @param bid
+     * @param data
+     * @return
+     */
     @RequestMapping(value = "/resource")
     public String resource(@RequestParam(value = "bid", required = false) String bid, ModelMap data) {
         data.put("bid", bid);
